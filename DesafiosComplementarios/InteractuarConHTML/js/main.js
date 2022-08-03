a79eb3eb3621d63cd85d9c3cac2a16efddb581f7
frnzDev <frnzDev@gmail.com>
2022-08-02 23:55:49 -0300

    Desafío complementario | Interactuar con HTML

---
+++ DesafiosComplementarios/InteractuarConHTML/js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("btn1-1").style.backgroundColor = "lightblue";
+    document.getElementById("btn1-2").style.backgroundColor = "black";
+    document.getElementById("btn1-3").style.backgroundColor = "green";
+    document.getElementById("btn2-1").style.backgroundColor = "blue";
+    document.getElementById("btn2-2").style.backgroundColor = "red";
+    document.getElementById("btn3-1").style.backgroundColor = "orange";
+}
