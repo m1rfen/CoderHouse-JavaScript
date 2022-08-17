f6a246fc12abd500a5e00f93200ce0afe0fe701a
frnzDev <frnzDev@gmail.com>
2022-08-16 23:56:33 -0300

    Segunda entrega en proceso | Proyecto final

---
+++ EntregasProyecto/SegundaEntrega/js/main.js
@@ -0,0 +1,39 @@
+const mouses = [
+    {
+        id: 1,
+        title: "Hati S Wireless Gaming Mouse",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/magic360/20314/20314-001.jpg",
+    },
+    {
+        id: 2,
+        title: "Hati S ACE Stardust Gaming Mouse",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/artiklar/zoom/19063_1.jpg?m=1623418074",
+    },
+    {
+        id: 3,
+        title: "Hati S Stardust Gaming Mouse",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/artiklar/zoom/100036_1.jpg?m=1597151138",
+    },
+    {
+        id: 4,
+        title: "Hati Gaming Mouse Limited Edition Stardust",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/artiklar/zoom/16245_1.jpg?m=1588925655",
+    },
+    {
+        id: 5,
+        title: "HSK Plus Fingertip Wireless Gaming Mouse",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/artiklar/zoom/21575_1.jpg?m=1652279999",
+    },
+    {
+        id: 6,
+        title: "Skoll Mini Gaming Mouse",
+        category: "mouse",
+        src: "https://www.maxgaming.com/bilder/artiklar/zoom/100045_1.jpg?m=1597153078",
+    }
+
+]
